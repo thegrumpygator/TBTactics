a6ca80cb5a4daac6f9a0993717d032c08c9ac65e
Brian <brian@grumpygator.us>
2015-11-07 11:47:49 -0500

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    // Use this for initialization
+    public Vector3 moveDestination;
+
+    void Awake()
+    {
+        moveDestination = transform.position;
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void TurnUpdate()
+    {
+
+    }
+
+}
